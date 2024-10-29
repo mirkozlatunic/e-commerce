e1c092599f2dfa003f82c0c67c41c44058dc95cf
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-10-28 22:37:56 -0700

    setup of header component

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,5 @@ module.exports = {
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
+  rules: { 'no-console': 'off' },
 }
---
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Header } from '../../../../payload/payload-types'
+
+const HeaderComponent = ({ header }: { header: Header }) => {
+  return <div>HeaderComponent</div>
+}
+
+export default HeaderComponent
