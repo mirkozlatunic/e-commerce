a777e564df3a68d5e0d5c0d1eeea0400cf6b6abf
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-10-29 21:12:10 -0700

    setup of logo on navbar

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -1,13 +1,9 @@
-{
-  /* eslint-disable @next/next/no-img-element */
-}
-
 import React from 'react'
 import Link from 'next/link'
 
-import { Header } from '../../../payload/payload-types'
+import type { Header } from '../../../payload/payload-types'
 import { fetchHeader } from '../../_api/fetchGlobals'
-import { HeaderComponent } from './HeaderComponent'
+import HeaderComponent from './HeaderComponent'
 
 export async function Header() {
   let header: Header | null = null
