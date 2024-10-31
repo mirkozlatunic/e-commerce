987da6e3a2d12ed6a19669e0c792a484c0a1cf11
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-10-30 21:14:38 -0700

    start setup of Navbar Mobile

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -6,6 +6,8 @@ import Link from 'next/link'
 
 import classes from './index.module.scss'
 import Image from 'next/image'
+import { HeaderNav } from '../Nav'
+import MobileNav from '../HeaderMobileNav'
 
 const HeaderComponent = ({ header }: { header: Header }) => {
   return (
@@ -14,6 +16,8 @@ const HeaderComponent = ({ header }: { header: Header }) => {
         <Link href="/">
           <Image src="/logo-black.svg" alt="logo" width={170} height={50} />
         </Link>
+        <HeaderNav header={header} />
+        <MobileNav header={header} />
       </Gutter>
     </nav>
   )
