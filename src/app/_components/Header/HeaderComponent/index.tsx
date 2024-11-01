680ed42ad6b788676976cf52f9a5b18a86305837
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-10-31 20:41:10 -0700

    Navbar completed

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,17 +1,27 @@
+'use client'
+
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 import { Header } from '../../../../payload/payload-types'
+import { noHeaderFooterUrls } from '../../../constants'
 import { Gutter } from '../../Gutter'
-import Link from 'next/link'
+import MobileNav from '../HeaderMobileNav'
+import { HeaderNav } from '../Nav'
 
 import classes from './index.module.scss'
-import Image from 'next/image'
-import { HeaderNav } from '../Nav'
-import MobileNav from '../HeaderMobileNav'
 
 const HeaderComponent = ({ header }: { header: Header }) => {
+  const pathname = usePathname()
+
   return (
-    <nav className={classes.header}>
+    <nav
+      className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
+        .filter(Boolean)
+        .join(' ')}
+    >
       <Gutter className={classes.wrap}>
         <Link href="/">
           <Image src="/logo-black.svg" alt="logo" width={170} height={50} />
