d13f280a0e4cb387f1883f1ca8dcd27d2fab58aa
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-11-07 10:20:23 -0800

    passkey setup completed

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,7 +16,12 @@ function Header() {
   const { user } = useUser();
 
   const createClerkPasskey = async () => {
-    await user?.createPasskey();
+    try {
+      const response = await user?.createPasskey();
+      console.log(response);
+    } catch (err) {
+      console.error('Error:', JSON.stringify(err, null, 2));
+    }
   };
 
   return (
@@ -82,7 +87,7 @@ function Header() {
                 onClick={createClerkPasskey}
                 className="bg-white hover:bg-blue-700 hover:text-white animate-pulse text-blue-500 font-bold py-2 px-4 rounded border-blue-300 border"
               >
-                Create passkey now
+                Create passkey
               </button>
             )}
           </ClerkLoaded>
