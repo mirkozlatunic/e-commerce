8eb8c0041675c9cc63cc52142bfcbcae48ec6471
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-11-20 22:10:18 -0800

    success page completed

--- actions/createCheckoutSession.ts
+++ actions/createCheckoutSession.ts
@@ -36,6 +36,14 @@ export async function createCheckoutSession(
       customerId = customer.data[0].id;
     }
 
+    const baseUrl =
+      process.env.NODE_ENV === 'production'
+        ? `https://${process.env.VERCEL_URL}`
+        : `${process.env.NEXT_PUBLIC_BASE_URL}`;
+
+    const successUrl = `${baseUrl}/success?session_id={CHECKOUT_SESSION_ID}&orderNumber=${metadata.orderNumber}`;
+    const cancelUrl = `${baseUrl}/basket`;
+
     const session = await stripe.checkout.sessions.create({
       customer: customerId,
       customer_creation: customerId ? undefined : 'always',
@@ -43,8 +51,8 @@ export async function createCheckoutSession(
       metadata,
       mode: 'payment',
       allow_promotion_codes: true,
-      success_url: `${`https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBLIC_BASE_URL}/success?session_id={CHECKOUT_SESSION_ID}&orderNumber=${metadata.orderNumber}`,
-      cancel_url: `${`https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBLIC_BASE_URL}/basket`,
+      success_url: successUrl,
+      cancel_url: cancelUrl,
       line_items: items.map((item) => ({
         price_data: {
           currency: 'usd',
